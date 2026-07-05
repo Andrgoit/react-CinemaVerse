b7ffa367a690d51db546c515fd3ce70f62524b6a
Andrgoit <stakgrom@gmail.com>
2026-07-05 17:17:53 +0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,16 @@
+import { createPortal } from "react-dom";
+import loaderImg from "@/assets/img/loader.png";
+import styles from "./Loader.module.css";
+
 export default function Loader() {
-  return <div>Loader</div>;
+  const root = document.getElementById("modal");
+
+  return createPortal(
+    <div className={styles.wrapper}>
+      <div className={styles.imgWrapper}>
+        <img src={loaderImg} alt="loader icon" className={styles.img} />
+      </div>
+    </div>,
+    root,
+  );
 }
